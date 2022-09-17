5a50023fb53ffcf828fbe8e96d44e588f0802ae8
Pini4 <User@WIN-GA1Q70LI9LP>
2022-09-17 18:39:45 +0300

    Было-Стало

--- Calculator-v2/Program.cs
+++ Calculator-v2/Program.cs
@@ -10,12 +10,87 @@
 
         double b = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Введи действие , которое должен выполнить калькулятор");
+        Console.WriteLine(@"Выберите действие , которое должен выполнить калькулятор:
+1 - сложить,
+2 - вычесть,
+3 - умножить,
+4 - делить,
+5 - синус,
+6 - косинус,
+7 - тангенс,
+8 - квадратный корень,
+9 - степень.");
 
-        var enter = Console.ReadKey();
-        char enter_key = enter.KeyChar;
 
-        if (enter_key == '+')
+
+        string action = Console.ReadLine();
+        switch (action)
+        {
+            case "1":
+                Console.WriteLine(a + b);
+                break;
+
+             case "2":
+                Console.WriteLine(a - b);
+                break;
+
+            case "3":
+                Console.WriteLine(a * b);
+                break;
+
+            case "4":
+                if (b == 0)
+                {
+                    Console.WriteLine("Дурак, на ноль делить нельзя");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(a / b);
+                    break;
+                }
+
+            case "5":
+                Console.Write("Укажите число , синус которого хотите узнать: ");
+                Console.WriteLine(Math.Sin(double.Parse(Console.ReadLine())));
+                break;
+
+            case "6":
+                Console.Write("Укажите число , косинус которого хотите узнать: ");
+                Console.WriteLine(Math.Cos(double.Parse(Console.ReadLine())));
+                break;
+
+            case "7":
+                Console.Write("Укажите число , тангенс которого хотите узнать: ");
+                Console.WriteLine(Math.Tan(double.Parse(Console.ReadLine())));
+                break;
+
+            case "8":
+                Console.Write("Укажите число , квадратный корень которого хотите узнать: ");
+                Console.WriteLine(Math.Sqrt(double.Parse(Console.ReadLine())));
+                break;
+            case "9":
+                Console.Write("Укажите число и степень через запятую: ");
+                string powinfo = Console.ReadLine();
+                var powsplit = powinfo.Split(',');
+                if (powsplit.Count() == 2)
+                {
+                    double n = double.Parse(powsplit[0].Trim());
+                    double m = double.Parse(powsplit[1].Trim());
+                    Console.WriteLine(Math.Pow(n, m));
+                }
+                else
+                    Console.WriteLine("Вы ввели неправильные значения");
+                break;
+            default:
+                
+                Console.WriteLine("Неверное действие");
+                return;
+                
+
+        }
+
+        /*if (enter_key == '+')
         {
             Console.WriteLine(a + b);
         }
@@ -49,6 +124,6 @@
         double v = Convert.ToDouble(Console.ReadLine());
         double m = Math.Pow(w, v);
         Console.WriteLine($" Число {w} в степени {v} равно {m}");
-
+        */
     }
 }
